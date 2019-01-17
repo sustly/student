4c854ad1e7fddbbef015234a307eaa1581fc5aa6
sustly <279719732@qq.com>
2019-01-17 11:37:27 +0800

    全部修改为springmvc，但是学生列表展示有问题

---
+++ src/main/java/com/sinuonan/controller/UpdateController.java
@@ -0,0 +1,46 @@
+package com.sinuonan.controller;
+
+import com.sinuonan.bean.StudentInfo;
+import com.sinuonan.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class UpdateController{
+
+
+    @Resource(name = "StudentService")
+    private StudentService service;
+
+
+    @RequestMapping(value = "update")
+    public String update(@RequestParam("schoolId") Integer schoolId,
+                         @RequestParam("name") String name,
+                         @RequestParam("gender") String gender,
+                         @RequestParam("className") String className,
+                         @RequestParam("uuid") String uuid,
+                         HttpServletRequest request){
+        StudentInfo info = new StudentInfo();
+        info.setSchoolId(schoolId);
+        info.setUuid(uuid);
+        info.setGender(gender);
+        info.setClassName(className);
+        info.setName(name);
+        info.setTeacherId((String) request.getSession().getAttribute("id"));
+        service.updateStudent(info);
+        return "student_list";
+    }
+
+    @RequestMapping(value = "updateGo")
+    public String updateGo(@RequestParam("uuid") String uuid,HttpServletRequest request){
+        List<StudentInfo> students = service.findStudentByUuid(uuid);
+        StudentInfo st = students.get(0);
+        request.setAttribute("st",st);
+        return "updateStudent";
+    }
+}
