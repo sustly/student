831c14de918d835e3e427148e4d52ff3350f68ae
sustly <279719732@qq.com>
2019-01-25 08:34:30 +0800

    添加一些校验

--- src/main/java/com/sinuonan/controller/LoginController.java
+++ src/main/java/com/sinuonan/controller/LoginController.java
@@ -41,7 +41,7 @@ public class LoginController{
             map.put("list",list);
             return "student_list";
         } else {
-            return "home/login";
+            return "login";
         }
     }
 
