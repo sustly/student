b20dfafaa3324a291787ddde42034efce58c2a92
sustly <279719732@qq.com>
2019-01-25 13:28:23 +0800

    添加一些校验

--- src/main/java/com/sustly/interceptors/LoginInterceptor.java
+++ src/main/java/com/sustly/interceptors/LoginInterceptor.java
@@ -8,26 +8,16 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
-    private static final String[] IGNORE_URI = {"/login"};
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
-        boolean flag = false;
-        String servletPath = request.getServletPath();
-        for(String s : IGNORE_URI) {
-            if(servletPath.contains(s)) {
-                flag = true;
-                break;
-            }
-        }
-        if(flag == false) {
+
             String id  = (String) request.getSession().getAttribute("id");
             if(id == null) {
                 response.sendRedirect("/home/login");
+                return false;
             }else {
-                flag = true;
+                return true;
             }
-        }
-        return flag;
     }
 
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
